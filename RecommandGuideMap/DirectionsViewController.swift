aa554a2c8d2b8f79adeee4b63d21229303c63b52
최재영 <chlwodud9807@naver.com>
2025-11-07 16:19:44 +0900

    feat: CLLocationManagerDelegate 구현, gpx파일 생성, DirectionsViewController파일 추가, plist에 Location Key 추가

---
+++ RecommandGuideMap/DirectionsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DirectionsViewController.swift
+//  RecommandGuideMap
+//
+//  Created by JaeYeongMAC on 11/7/25.
+//
+
+import UIKit
+
+class DirectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
